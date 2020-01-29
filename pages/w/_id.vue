741937891aa2001b6ecce9acdb316bf3f47129c6
Horus <horus20@mail.ru>
2020-01-29 17:06:15 +0700

    fix

--- pages/w/_id.vue
+++ pages/w/_id.vue
@@ -292,8 +292,8 @@
   import VueQrcode from '@chenfengyuan/vue-qrcode'
   import * as cryptoRandomString from 'crypto-random-string'
 
-  //const BACKEND_BASE_URL = 'https://minterpush.ru'
-  const BACKEND_BASE_URL = 'http://localhost:3048'
+  const BACKEND_BASE_URL = 'https://minterpush.ru'
+  //const BACKEND_BASE_URL = 'http://localhost:3048'
   const EXPLORER_BASE_URL = 'https://explorer-api.minter.network'
   const EXPLORER_GATE_API_URL = 'https://gate-api.minter.network'
   const LINK = 'https://minterpush.ru/w/'
