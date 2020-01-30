4ca1fb1b7d1ba74abada416b8f11936b67e1a8ff
Horus <horus20@mail.ru>
2020-01-30 09:43:35 +0700

    fix style

--- pages/w/_id.vue
+++ pages/w/_id.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <div class="row header-section">
       <div class="col-2"><b-img src="/minter-logo-circle.png" fluid alt="" style="max-width: 36px; margin-top: 3px;"></b-img></div>
-      <div class="col-7 text-center"><span class="font-weight-bold">#{{ uid }}</span></div>
-      <div class="col-3 language-box text-right"><span class="">RU</span></div>
+      <div class="col-7 text-center" style="padding-top: 8px;"><span class="font-weight-bold">#{{ uid }}</span></div>
+      <div class="col-3 language-box text-right" style="padding-top: 8px;"><span class="">RU</span></div>
     </div>
 
     <template v-if="isLogin">
