926eb01611fd48d8565b1800e71359f3504bd19a
Horus <horus20@mail.ru>
2020-01-31 13:41:24 +0700

    fix

--- pages/w/_id.vue
+++ pages/w/_id.vue
@@ -324,8 +324,8 @@
     Vue.use(VueClipboard)
   }
 
-  //const BACKEND_BASE_URL = 'https://minterpush.ru'
-  const BACKEND_BASE_URL = 'http://localhost:3048'
+  const BACKEND_BASE_URL = 'https://minterpush.ru'
+  //const BACKEND_BASE_URL = 'http://localhost:3048'
   const EXPLORER_BASE_URL = 'https://explorer-api.minter.network'
   const EXPLORER_GATE_API_URL = 'https://gate-api.minter.network'
   const LINK = 'https://minterpush.ru/w/'
